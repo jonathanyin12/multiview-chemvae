19996b743ccf1930c9d709c87a8bd07f9e141c33
Jonathan Yin <jonathanyin.12@gmail.com>
2021-08-03 00:27:05 -0700

    Reformat prediction output

--- prediction_utils.py
+++ prediction_utils.py
@@ -46,28 +46,32 @@ def get_multilabel_ROC_score(X,y,latent_rep_size, output_size, folds=10):
             aucs.append(score)
         except:
             continue
-    print(np.average(aucs), np.std(aucs))
-
+    print("Mean AUC-ROC: {} =/- {}".format(np.average(aucs), np.std(aucs)))
 
 
 def rmse(y_pred, y_true):
     return K.sqrt(K.mean(K.square(y_pred - y_true)))
 
+
 def get_regression_loss(X,y,latent_rep_size, loss_metric, folds=10):
     kfold = KFold(n_splits=folds, shuffle=True, random_state=0)
     losses = []
     for train, test in tqdm(kfold.split(X, y), total=folds):
-        model=create_model(input_size=latent_rep_size, activation="linear", loss=loss_metric)
-
+        if loss_metric=='rmse':
+            model=create_model(input_size=latent_rep_size, activation="linear", loss=rmse)
+        else:
+            model=create_model(input_size=latent_rep_size, activation="linear", loss=loss_metric)
         model.fit(X[train], y[train], epochs=250, batch_size=256, validation_split=0.1, verbose=0, callbacks = [early_stop])
         loss=model.evaluate(X[test], y[test], verbose=0)
         losses.append(loss)
-    print(np.average(losses), np.std(losses))
-    
+
+    print("Mean {}: {} +/- {}".format(loss_metric, np.average(losses), np.std(losses)))    
+
 
 def get_ROC_score(X, y, latent_rep_size, folds=10):
     base_fpr, mean_tprs, mean_auc, std_auc, tprs_lower, tprs_upper = get_average_ROC_curve(X, y, latent_rep_size, folds=10)
-    print(mean_auc, std_auc)
+    print("Mean AUC-ROC: {} +/- {}".format(mean_auc, std_auc))
+
 
 def get_average_ROC_curve(X, y, latent_rep_size, folds=10): 
     kfold = StratifiedKFold(n_splits=folds, shuffle=True, random_state=0)
@@ -106,6 +110,7 @@ def get_average_ROC_curve(X, y, latent_rep_size, folds=10):
 
     return base_fpr, mean_tprs, mean_auc, std_auc, tprs_lower, tprs_upper
  
+
 def plot_average_ROC_curves(roc_curves, dataset_name, model_names, folds=10,):
 
     plt.figure(figsize=(8, 6))
